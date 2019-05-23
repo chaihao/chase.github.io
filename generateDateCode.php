3d36bea0b39729c9f7b385ac0da665467ac65b33
柴浩 <158533752@qq.com>
2019-05-24 00:02:38 +0800

    事务

--- generateDateCode.php
+++ generateDateCode.php
@@ -13,7 +13,7 @@
         // key 中的域 field 
         $code = $data['code'] ? $data['code'] : 'code';
         $field =  $code . $date;
-        
+
         $id = Yii::$app->redis->hincrby($key, $field, 1);
         // 数值长度
         $length = $data['code'] ? $data['code'] : '10';
@@ -25,6 +25,25 @@
         return $serialNumber;
     }
  
+    // 事务
+    public function trans($logicFn, $info)
+    {
+        $info = sprintf('执行事务%s', $info);
+        // begin
+        $transaction = static::getDb()->beginTransaction();
+        try {
+            $result = call_user_func($logicFn);
+            // commit
+            $transaction->commit();
+            Yii::info($info . '成功');
+        } catch (yii\db\Exception $e) {
+            // rollback
+            $transaction->rollBack();
+            Yii::warning($info . '失败, ' . $e->getMessage());
+            $result = $e->getMessage();
+        }
+        return $this->result($result);
+    }
 }
 
 
